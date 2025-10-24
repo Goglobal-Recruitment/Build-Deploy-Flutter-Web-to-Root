c90113104acb6f88f1c576c5cd9335931732a834
Go-Global <interviewsglo@outlook.com>
2025-10-24 17:23:29 +0200

    ,

--- lib/lib/services/lib/services/lib/api_keys.dart
+++
@@ -1,4 +0,0 @@
-class ApiKeys {
-  static const String serpApiKey = "907510a136aa1302c59c220d803d7d19fcf140b1588c449a079896d03b1a654d";
-  static const String paystackPublicKey = "pk_test_your_public_key_here"; // pk_live_29f44e65db94a7f8bf959a8ce9bf06ed0bffa720
-}
